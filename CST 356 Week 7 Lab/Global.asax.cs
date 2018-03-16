ac41938553f72bfa99b8b6578a6a5b03becc69c4
Nicholas Olson <latentdev@gmail.com>
2018-03-16 04:29:21 -0700

    Initial Commit

--- CST 356 Week 7 Lab/Global.asax.cs
+++ CST 356 Week 7 Lab/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CST_356_Week_7_Lab.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace CST_356_Week_7_Lab
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DependencyInjectionConfig.Register();
         }
     }
 }
